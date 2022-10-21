2012fba9499a016355faefb691114c2ce3f38325
taptap-kor <tyy8282@naver.com>
2022-10-21 12:46:31 +0900

    restructure

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,18 +1,27 @@
 import "./App.css";
-import MetamaskConnect from "./apps/MetamaskConnect";
-import NFTMint from "./apps/NFTMint";
-import NFTList from "./apps/NFTList";
-import MyPage from "./apps/MyPage";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import MintPage from "./pages/MintPage"
+// import MetamaskConnect from "./component/MetamaskConnect";
+// import NFTMint from "./component/NFTMint";
+// import NFTList from "./component/NFTList";
+// import MyPage from "./pages/MintPage";
 
 function App() {
   return (
-    <div>
-      <h1>CheckShirts Scan</h1>
-      <MetamaskConnect></MetamaskConnect>
-      <NFTMint />
-      <NFTList />
-      <MyPage />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route path="/mintpage" element={<MintPage />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+    // <div>
+    //   <h1>CheckShirts Scan</h1>
+    //   <MetamaskConnect></MetamaskConnect>
+    //   <NFTMint />
+    //   <NFTList />
+    //   <MyPage />
+    // </div>
   );
 }
 
---
+++ Client/src/component/Loading.js
@@ -0,0 +1,24 @@
+import React from "react";
+import styled from "styled-components";
+import "./loading.css";
+
+const LoadingContainer = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+const LoadingBox = styled.div`
+  width: 300px;
+  height: 300px;
+`;
+
+const Loading = () => {
+  return (
+    <LoadingContainer>
+      <LoadingBox>
+        <div className="dot-flashing"></div>
+      </LoadingBox>
+    </LoadingContainer>
+  );
+};
+
+export default Loading;
